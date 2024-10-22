5cc728516cf1cd430d2bc07896e59959c6c72725
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-22 17:09:30 -0500

    Time: 0 ms (100.00%) | Memory: 23.8 MB (97.43%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,10 +1,32 @@
-from collections import Counter
+# from collections import Counter
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        hashmap = {}
+        # count = Counter(nums)
 
-        count = Counter(nums)
+        # for num, count in count.items():
+        #     if count ==1:
+        #         return num  #O(n) time and space 
 
-        for num, count in count.items():
-            if count ==1:
-                return num
+
+        #Binary search Tree for O(logn) time:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = left + (right-left)//2
+
+            #check mid is within bounds and is unique by checking adjacent elements
+            if ((mid -1 < 0 or nums[mid] != nums[mid-1]) and 
+            (mid+1 == len(nums) or nums[mid] != nums[mid+1])):
+                return nums[mid]
+            
+            # size of the left side
+            left_size = mid -1 if nums[mid-1] == nums[mid] else mid
+
+            #left size is odd, so single element is in the first half
+            if left_size %2:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+
+
