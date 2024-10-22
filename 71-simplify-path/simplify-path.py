eb4f8c836394af9821a573974fb2927681de0641
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-22 00:21:10 -0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (72.30%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -2,10 +2,10 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
 
-        for p in path.split('/'):
-            if stack and p == "..":
-                stack.pop()  #pop elements if .. is the component while splitting
-            elif p not in [".", "", ".."]:
-                stack.append(p)
+        for i in path.split('/'):
+            if stack and i == "..":
+                stack.pop()  #pop elements if .. as it says to come out (pop) of the directory in the path
+            elif i not in [".", "", ".."]:
+                stack.append(i)
         
         return '/' + '/'.join(stack)
