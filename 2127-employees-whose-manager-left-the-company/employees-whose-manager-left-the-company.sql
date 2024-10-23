1d69a98e616d5b7ad131b251f1eef7ddff204b49
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-23 10:19:48 -0500

    Time: 362 ms (29.28%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,7 +1,8 @@
 #id's whose salayary < 30000 and manager left the company- manager leaves, info deleted from employees table
 #return ordered by employee id
 
-select employee_id from employees
+select employee_id 
+from employees
 where salary < '30000' and
 manager_id not in (select employee_id from employees)
-order by employee_id;
+order by employee_id asc;
