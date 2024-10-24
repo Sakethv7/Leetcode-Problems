20336a6f8bebb145b41bee6b4d4134f42d2daed9
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-24 12:01:56 -0500

    Time: 393 ms (21.48%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -7,7 +7,12 @@ select
     conditions
 from 
     patients
-    where conditions regexp '(^| )DIAB1[0-9]*';
+    where conditions regexp '(^| )DIAB1[0-9]*(|$)';
 
     #regexp returns the query which has the expression 'x': where row_name regexp 'x'
-
+    #
+-- Matches either the start of the string (^) or a space ( ), ensuring that DIAB1 is either at the start or preceded by a space.
+-- DIAB1:
+-- Matches the literal string "DIAB1".
+-- [0-9]*:
+-- Matches zero or more digits following "DIAB1".
