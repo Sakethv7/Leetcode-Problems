051e9374569ec2f04cb7b16f4cb3dc57013dbe2c
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-27 18:32:35 -0500

    Time: 3 ms (88.98%) | Memory: 18.3 MB (6.35%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
+        #return total number of provinces: provice is a group directly or indirectly connected. 
+        #so we need to find the number of components in a graph       
+
+        #using recursion
+        
+        def dfs(i):
+            visited.add(i)
+            for j in range(n):
+                if j not in visited and isConnected[i][j] == 1:  #theres an edge i, j where i is directly connected to j
+                    dfs(j)
+
+        n = len(isConnected) #get length of nodes
+        visited = set() 
+        components = 0
+
+        for i in range(n): # 0 to n-1
+            if i not in visited:
+                dfs(i)
+                components+=1
+        
+        return components
+
+        #time complexity O(n^2)        
