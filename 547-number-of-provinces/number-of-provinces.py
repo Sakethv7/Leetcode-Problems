4518f572979a4fc660c912bee1971b548426adca
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-27 18:37:01 -0500

    Time: 31 ms (10.36%) | Memory: 17.9 MB (16.80%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -5,23 +5,43 @@ class Solution:
         #return total number of provinces: provice is a group directly or indirectly connected. 
         #so we need to find the number of components in a graph       
 
-        #using recursion
-        
-        def dfs(i):
-            visited.add(i)
-            for j in range(n):
-                if j not in visited and isConnected[i][j] == 1:  #theres an edge i, j where i is directly connected to j
-                    dfs(j)
-
+        #using stack dfs
         n = len(isConnected) #get length of nodes
         visited = set() 
         components = 0
 
-        for i in range(n): # 0 to n-1
+        for i in range(n):
             if i not in visited:
-                dfs(i)
+                stack = [i]
+                
+                while stack:
+                    node = stack.pop()
+                    visited.add(node)
+                    for j in range(n):
+                        if j not in visited and isConnected[node][j] ==1:
+                            stack.append(j)
+                        
                 components+=1
         
         return components
 
-        #time complexity O(n^2)        
+        #using recursion
+        
+        # def dfs(i):
+        #     visited.add(i)
+        #     for j in range(n):
+        #         if j not in visited and isConnected[i][j] == 1:  #theres an edge i, j where i is directly connected to j
+        #             dfs(j)
+
+        # n = len(isConnected) #get length of nodes
+        # visited = set() 
+        # components = 0
+
+        # for i in range(n): # 0 to n-1
+        #     if i not in visited:
+        #         dfs(i)
+        #         components+=1
+        
+        # return components
+
+        # #time complexity O(n^2)        
