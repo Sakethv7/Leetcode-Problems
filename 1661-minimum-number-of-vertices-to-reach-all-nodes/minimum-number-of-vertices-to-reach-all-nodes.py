29f45db3b9077698667000e2e7cc1a03116f45ff
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-27 19:06:53 -0500

    Time: 20 ms (65.55%) | Memory: 43.5 MB (97.26%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -4,15 +4,16 @@ class Solution:
 
         #basically in a directed acyclic graph, for smallest set of verticies with which all nodes in the
         # graph are reachable. the set of vertices would be the ones that do not have any incoming edges
-        
+
         visited = set()
-        for i in edges:
+        for i in edges: #edges is a list given, that has the edges that are connected where dges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
+        
             visited.add(i[1]) #add destination node into the visited list.
         
         result = []
         for j in range(n): #iterate over all nodes 
-            if j not in visited:
-                result.append(j)
+            if j not in visited: # if a node is not in visited set that means it is a node with no incoming edge
+                result.append(j) 
         
         return result  
 
