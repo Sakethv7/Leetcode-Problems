a8babd74bbd1bf0d62ecc4416981e21318dff80f
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-10-30 18:25:30 -0500

    Time: 65 ms (57.69%) | Memory: 17.4 MB (18.21%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -2,19 +2,19 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         num_zeroes = 0
-        windowstart = 0
+        window_start = 0
+        n = len(nums)
         max_length = 0
 
-        for windowend in range(len(nums)):
-            if nums[windowend] == 0:
+        for windowend in range(n):
+            if nums[windowend] == 0:  # If encounter a zero, increment the zero count
                 num_zeroes +=1
-    
+        # Shrink the window until the zero count is within the allowed limit
             while num_zeroes >k:
-                if nums[windowstart] ==0:
-                   num_zeroes -=1
-                windowstart +=1
-
-            max_length = max(max_length, windowend-windowstart+1)
+                if nums[window_start]==0:
+                    num_zeroes -=1
+                window_start += 1
+            
+            max_length = max(max_length, windowend - window_start +1)   # Update max_length for the current valid window
         
         return max_length
-
