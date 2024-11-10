5951b14e92794cfbf63741c43bb208f832287ef0
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-11-10 09:31:26 -0600

    Time: 0 ms (100.00%) | Memory: 16.7 MB (20.52%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,15 +1,25 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        res = [1] # Initialize the first row of Pascal's triangle
+        # res = [1] # Initialize the first row of Pascal's triangle
 
-        for i in range(rowIndex):  # Generate rows up to the specified rowIndex
-            next_row = [0]*(len(res)+1)   # Create a new row with an additional element, initialized to 0
+        # for i in range(rowIndex):  # Generate rows up to the specified rowIndex
+        #     next_row = [0]*(len(res)+1)   # Create a new row with an additional element, initialized to 0
 
-            for j in range(len(res)): # Populate next_row by adding elements from the previous row
-                next_row[j] += res[j]
-                next_row[j+1] += res[j]
+        #     for j in range(len(res)): # Populate next_row by adding elements from the previous row
+        #         next_row[j] += res[j]
+        #         next_row[j+1] += res[j]
             
-            res = next_row  # Update res to the new row
+        #     res = next_row  # Update res to the new row
 
-        return res
+        # return res
         
+
+        # optimized version using O(rowIndex) extra space
+
+        res = [1] * (rowIndex+1)
+
+        for i in range(1, rowIndex+1):
+            for j in range(i-1, 0, -1):    # Update from the end to the beginning to avoid overwriting needed values
+                res[j] += res[j-1]
+        
+        return res
