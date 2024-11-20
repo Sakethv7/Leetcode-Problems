94362315dd23ad6db07e81cd00317c05a81c15e1
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-11-20 13:52:27 -0600

    Time: 3 ms (20.06%) | Memory: 16.9 MB (11.71%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -1,25 +1,42 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        # return the kth integer missing from this array- already sorted array, k is the index of the missing number
+        # # return the kth integer missing from this array- already sorted array, k is the index of the missing number
 
-        # can we use binary search? take a mid point? if the number is bigger
+        # # can we use binary search? take a mid point? if the number is bigger
 
-        # o(logn)
+        # # o(logn)
 
-        n = len(arr)
-        left, right = 0, n-1
+        # n = len(arr)
+        # left, right = 0, n-1
 
-        while left<=right:
+        # while left<=right:
 
-            mid = (left+right)//2
+        #     mid = (left+right)//2
 
-            missing = arr[mid] - (mid + 1) #number of missing integers before arr[mid]
+        #     missing = arr[mid] - (mid + 1) #number of missing integers before arr[mid]
 
-            if missing < k:
-                left = mid+1
-            else:
-                right = mid-1
+        #     if missing < k:
+        #         left = mid+1
+        #     else:
+        #         right = mid-1
 
         
-        return k+left
+        # return k+left
+
+        #O(n)
+
+        missing = 0
+        current =1
+        index =0
+
+        while missing < k:
+            if index < len(arr) and arr[index] == current:
+                index += 1
+            else:
+                missing += 1
+                if missing == k:
+                    return current
+            current += 1
+        
+        return _
 
