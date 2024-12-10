b9f875eb442e3708ea2922b839fe837f1eeb09c4
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-12-10 01:10:26 -0600

    Time: 60 ms (13.06%) | Memory: 17.5 MB (7.69%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.py
+++ 3463-alternating-groups-i/alternating-groups-i.py
@@ -2,15 +2,30 @@ class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
         #return the number of alternating colors 
 
+        # count = 0
+        # n = len(colors)
+
+        # for i in range(len(colors)):
+        #     if i== 0 and colors[i] != colors[n-1] and colors[i] != colors[i+1]:
+        #         count += 1
+        #     elif i == n-1 and colors[i] != colors[0] and colors[i] != colors[i-1]:
+        #         count += 1
+        #     elif i != 0 and i != n-1 and colors[i] != colors[i-1] and colors[i] != colors[i+1]:
+        #         count += 1
+
+        # return count 
+
+        #modular approach:
+
         count = 0
         n = len(colors)
-        
-        for i in range(len(colors)):
-            if i== 0 and colors[i] != colors[n-1] and colors[i] != colors[i+1]:
-                count += 1
-            elif i == n-1 and colors[i] != colors[0] and colors[i] != colors[i-1]:
-                count += 1
-            elif i != 0 and i != n-1 and colors[i] != colors[i-1] and colors[i] != colors[i+1]:
-                count += 1
 
-        return count 
+        for i in range(n):
+            prev_color = colors[i-1]
+            next_color = colors[(i+1) % n]
+
+            if colors[i] != prev_color and colors[i]!= next_color:
+                count += 1
+        
+        return count
+        
