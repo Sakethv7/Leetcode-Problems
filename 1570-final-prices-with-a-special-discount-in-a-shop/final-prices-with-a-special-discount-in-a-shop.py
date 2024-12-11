fef2d05904a2be2edbf0ebb91e558df64aaf1ea8
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-12-10 23:42:09 -0600

    Time: 3 ms (55.64%) | Memory: 17.6 MB (8.77%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,15 +1,26 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        answer = []
+        answer = prices[:]
         n = len(prices)
-        
+        stack = []
+
         for i in range(n):
-            discount = 0
-            for j in range(i+1, n):
-                if prices[j] <= prices[i]:
-                    discount = prices[j]
-                    break
+            while stack and prices[stack[-1]] >= prices[i]:  # The price at the index on top of the stack is greater than or equal to the current price (prices[i]). This means the current price can serve as a discount.
+                index = stack.pop()
+                answer[index] -= prices[i]
             
-            answer.append(prices[i]-discount)
+            stack.append(i)
         
         return answer
+        # #O(n^2) method
+        # for i in range(n):
+        #     discount = 0
+        #     for j in range(i+1, n):
+        #         if prices[j] <= prices[i]:
+        #             discount = prices[j]
+        #             break
+            
+        #     answer.append(prices[i]-discount)
+        
+        # return answer
+
