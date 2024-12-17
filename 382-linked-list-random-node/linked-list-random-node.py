6175e7e9e551539bcb8467298bddc82e526eac4a
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-12-17 16:05:43 -0700

    Time: 3 ms (97.75%) | Memory: 20.4 MB (5.47%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.py
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -5,24 +5,30 @@
 #         self.next = next
 class Solution:
 
-    def __init__(self, head: Optional[ListNode]):
-        self.head = head
+    def __init__(self, head: Optional[ListNode]):       
+        self.array = []
+        while head: 
+            self.array.append(head.val)
+            head = head.next
 
-    def getRandom(self) -> int:
-        count = 0
-        result = None
-        current = self.head
-
-        while current is not None:
-            count += 1
-
-            if random.randint(1, count) == 1:
-                result = current.val 
-            current = current.next
+    def getRandom(self):
+        return random.choice(self.array)
         
-        return result
-        
+        #     self.head = head
 
+    # def getRandom(self) -> int:
+    #     count = 0
+    #     result = None
+    #     current = self.head
+
+    #     while current is not None:
+    #         count += 1
+
+    #         if random.randint(1, count) == 1:
+    #             result = current.val 
+    #         current = current.next
+        
+    #     return result
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(head)
