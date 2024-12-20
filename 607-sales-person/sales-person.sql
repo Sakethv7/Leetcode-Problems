62b0d3f8a76398b1f158153affcce97071fb38f6
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2024-12-19 23:03:44 -0700

    Time: 2508 ms (7.85%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
 # find the names of salespersons with no orders of company name red
 
-select s.name 
-from orders o 
-join company c
-on (o.com_id = c.com_id and c.name = 'Red') # this table joined on orders and company has no other data except for the data that has name as red
-right join salesperson s # it then right joins on salesperson  
-on s.sales_id =  o.sales_id
-where o.sales_id is null
+-- select s.name 
+-- from orders o 
+-- join company c
+-- on (o.com_id = c.com_id and c.name = 'Red') # this table joined on orders and company has no other data except for the data that has name as red
+-- right join salesperson s # it then right joins on salesperson  
+-- on s.sales_id =  o.sales_id
+-- where o.sales_id is null
+
+
+select s.name from Salesperson s 
+where s.sales_id not in 
+(select o.sales_id from orders o left join company c on o.com_id = c.com_id where c.name = 'RED')
