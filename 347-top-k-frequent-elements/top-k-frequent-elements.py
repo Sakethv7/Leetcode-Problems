86972bc20678943362073143f82047c97b890bd2
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2025-01-11 16:00:01 -0700

    Time: 3 ms (92.06%) | Memory: 21.4 MB (28.06%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,14 +6,14 @@ class Solution:
         buckets = [[] for _ in range(max_freq + 1)]  # Create buckets from 0 to max_freq
 
         for num, count in freq.items(): # grouping num and count together grouped on the basis of their respective counts
-            buckets[count].append(num) 
+            buckets[count].append(num)  # buckets represent frequencies, thus frequencies are ordered from low to high
             #freq = Counter([1, 1, 1, 2, 2, 3])
             # freq = {1: 3, 2: 2, 3: 1}
         
         res = []
-        for i in range(max_freq, 0, -1): # Traverse buckets from high to low frequency because buckets become ordered in ascending so we get the largest frequencies at the end. 
-
+         # Traverse buckets from high to low frequency because buckets become ordered in ascending so we get the largest frequencies at the end. 
         #max_freq = len(buckets) - 1
+        for i in range(max_freq, 0, -1):
             for num in buckets[i]: 
                 res.append(num)
                 if len(res) == k:
