4dd77f4566e69e079f3ed7d74d933adfc38a4a72
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2025-01-21 00:50:24 -0700

    Time: 8 ms (53.44%) | Memory: 18.1 MB (45.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,22 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #if a character is not alpha or numerical, increment left pointer and decrement right pointer
-        # if left character == right character ( need to be lowercase as well)
+        left, right = 0, len(s) - 1
 
-        left = 0
-        right = len(s)-1
-  
-        while left<right:
-            if not s[left].isalnum():  # Move the left pointer until we find an alphanumeric character
-                left +=1
-
-            elif not s[right].isalnum(): # Move the right pointer until we find an alphanumeric character
-                right -=1
-            
-            elif s[left].lower() != s[right].lower(): # Compare characters
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
                 return False
-            else:     # Move pointers inward only if characters match
-                left +=1  
-                right -=1
-        
+            else:
+                left += 1
+                right -= 1
+    
         return True
