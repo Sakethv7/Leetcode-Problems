4dc458274e3c6082bfc71a89ebe6a5a752ad7298
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2025-04-19 22:04:02 -0400

    Time: 0 ms (100.00%) | Memory: 23.3 MB (38.52%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        
+        if not root:
+            return 0
+        
+        def get_depth(node, go_left):
+            depth = 0
+            while node:
+                node = node.left if go_left else node.right
+                depth += 1
+            
+            return depth
+
+        left_depth = get_depth(root, True)
+        right_depth = get_depth(root, False)
+
+        if left_depth == right_depth:
+            return 2**left_depth - 1
+        
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        
