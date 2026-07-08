785f304214a7a0f5ef7cefac78f0e093b7564f67
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2026-07-08 16:36:00 -0400

    Time: 4 ms (44.59%) | Memory: 21.3 MB (6.66%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,18 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i, n = 0, len(nums)
+        hashset = set(nums)
+        rng = len(nums)+1
 
-        while i < n:
-            j = nums[i]
-
-            if nums[i] < n and nums[i] != nums[j]:
-                nums[i], nums[j] = nums[j], nums[i]
-
-            else:
-                i += 1
-        
-        for i in range(n):
-            if nums[i] != i:
-                return i
-
-        return n
+        for number in range(rng):
+            if number not in hashset:
+                return number
