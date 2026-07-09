163258abc1856cf97cb953e569ce6f49fed9e7ed
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2026-07-08 23:23:39 -0400

    Time: 19 ms (75.36%) | Memory: 25.7 MB (44.69%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,18 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums)) # creatng an answer array which is of the size n of nums
+        product = 1
 
-        prefix = 1                           #initialising a prefix value 
-        for i in range(len(nums)):          #from the start of nums iterate through, Iterate through nums from left to right
-            res[i] = prefix                 # Assign the current prefix value to res[i]
-            prefix = prefix * nums[i]       # Update the prefix by multiplying it with the current element
+        n = len(nums)
+
+        result = [1]*n
+        prefix = 1
+
+        for i in range(n):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
         
-        postfix = 1                             #initializing a postfix value
-        for j in range(len(nums)-1, -1, -1):    #loop starts from the end of nums, Iterate through nums from right to left
-            res[j] = res[j]*postfix            #Multiply the current res[j] value with the postfix
-            postfix = postfix * nums[j]        #Update the postfix by multiplying it with the current element
+        for i in range(n-1, -1, -1):
+            result[i] *= suffix 
+            suffix *= nums[i]
         
-        return res
-
-
-
+        return result
