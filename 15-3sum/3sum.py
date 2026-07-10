38843c76f6ec2031f63cc4f14caf70f678d8bd0f
Saketh Velidimalla <132528528+Sakethv7@users.noreply.github.com>
2026-07-09 23:46:22 -0400

    Time: 652 ms (41.97%) | Memory: 22 MB (93.86%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,39 +1,40 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-# i!=j, j!=k and i!=k. nums[i]+nums[j] + nums[k] ==0, return nums[i, j , k] can 2 , can be 1 array. this is in a list
-        #sorted solution
-
-        nums.sort()
-        ans = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         n = len(nums)
+        s = 0
+        ans = []
+
+        if n< 3:
+            return []
+        
+        nums.sort()
 
         for i in range(n-2):
-            # Skip duplicate elements for the first number in the triplet
-            if i>0 and nums[i] == nums[i-1]:
+            if i >0 and nums[i] == nums[i-1]:
                 continue
-            
-            left, right = i+1, n-1
-        # Iterate through each element, using it as the fixed element in the triplet
+            left = i+1
+            right = n-1
+
             while left< right:
-                current_sum = nums[i] + nums[left] + nums[right]
+                s = nums[i] + nums[left] + nums[right]
 
-                if current_sum == 0:
-                    ans.append([nums[i], nums[left], nums[right]])
-        # Move both pointers to continue searching for new pairs without duplicates
-                    left+=1 
-                    right -=1
-        # Skip duplicate values for the `left` pointer to avoid duplicate triplets
-                    while left< right and nums[left]== nums[left-1]:
-                        left+=1
-        # Skip duplicate values for the `right` pointer to avoid duplicate triplets
-                    while left < right and nums[right] == nums[right+1]:
-                        right -=1
+                if s == 0:
+                    ans.append((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -=1 
+                    while nums[left] == nums[left-1] and left<right:
+                        left += 1
+                    while nums[right] == nums[right+1] and left < right:
+                        right -= 1
+                elif s<0:
+                    left += 1
+                else:
+                    right -= 1
         
-        
-                elif current_sum < 0: # If the current sum is less than zero, increment left to increase the sum
-                    left+=1
-        
-                else: # If the current sum is greater than zero, decrement `right` to decrease the sum
-                    right -=1
-
         return ans
+
+
+
+
+
+
